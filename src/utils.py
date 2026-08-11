6dca1fbfe3ac1b2c5aa45fb38093f10a4012329c
George Prince <georgeprince3787@gmail.com>
2026-08-11 19:20:12 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,44 @@
-import os
+import os 
 import sys
-import pickle
+try:
+    import dill
+except ImportError:
+    import pickle as dill
+from sklearn.metrics import r2_score
 
 import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
 
-
-def save_object(file_path, obj):
+def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, 'wb') as file_obj:
-            pickle.dump(obj, file_obj)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
     except Exception as ex:
-        raise CustomException(ex, sys)
+        raise CustomException(ex,sys)
+
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+
+            y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as ex:
+        raise CustomException(ex,sys)
