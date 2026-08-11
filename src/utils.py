d19274298850532f11ead9c70dd3b8089a627490
George Prince <georgeprince3787@gmail.com>
2026-08-11 23:02:32 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
     import pickle as dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 import numpy as np
 import pandas as pd
@@ -24,12 +25,20 @@ def save_object(file_path,obj):
         raise CustomException(ex,sys)
 
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param=None):
     try:
         report = {}
+        if param is None:
+            param = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = param.get(list(models.keys())[i], {})
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_test_pred = model.predict(X_test)
